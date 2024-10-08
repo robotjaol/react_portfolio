f414ca8cdef6db3f6b171b743c53017e134aa8fe
= <jonathanoktavianofrizzy@gmail.com>
2024-10-09 01:51:44 +0700

    link

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.scss";
-import { Person, Mail } from "@material-ui/icons";
+import { Person, Mail, LinkedIn, Email } from "@material-ui/icons";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
@@ -11,15 +11,25 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+62 813 5747 3781</span>
+            <a href="https://wa.me/081357473781">
+              <span>+62 813 5747 3781</span>
+            </a>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>jonathanoktavianofrizzy@gmail.com</span>
+            <a href="mailto:jonathanoktavianofrizzy@gmail.com">
+              <span>jonathanoktavianofrizzy@gmail.com</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <LinkedIn className="icon" />
+            <a href="https://linkedin.com/in/jonathan-oktaviano/">
+              <span>Jonathan Oktaviano Frizzy</span>
+            </a>
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
